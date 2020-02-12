34b5dca56988cea87ca542253ed9721817348f60
Grizz <21078047+iGrizz@users.noreply.github.com>
2020-02-12 17:04:27 +0800

    v0.0.1
    本地的Git出了点问题，用上传吧。。。

---
+++ README.md
@@ -0,0 +1,9 @@
+#PHP-Port-Test
+Requiements: 
+	PHP function: fsockopen
+
+For example: 
+	request: 
+		?h=byxong.com&port=22
+	result: (json)
+		{"host":"byxong.com","port":"22","result":"Closed"}
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-Type:text/json;charset=utf-8');
+$query = $_SERVER["QUERY_STRING"];
+parse_str($query);
+parse_str($query, $query); 
+$fp = @fsockopen($h,$p,$errno,$errstr,0.1);
+if (!$fp) {
+	$result = "Closed";
+    }else{
+	$result = "Opend";
+}
+$feedback = array
+       (
+          'host'=>$h,
+          'port'=>$p,
+          'result'=>$result
+       );
+$jsonback = json_encode($feedback);
+echo $jsonback;
+?>
